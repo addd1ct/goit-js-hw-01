d50e02856c03de1a4cc91188086cfad110c44fdd
polly <polifibliss@gmail.com>
2024-10-31 01:00:18 +0200

    update

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,5 @@
 function getShippingMessage(country, price, deliveryFee) {
-    let totalPrice = price + deliveryFee;
-    console.log('Shipping to ${country} will cost ${totalPrice} credits');
+    console.log(`Shipping to ${country} will cost ${price + deliveryFee} credits`);
 }
 
 console.log(getShippingMessage("Australia", 120, 50));
